cad9d38d2829e2cca4c331788c1325149a66fca7
Diogo Ide <diogoide@gmail.com>
2018-04-04 00:36:45 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+end
+
+my_each(array) do |collection|
+  puts "#{collection}"
 end
